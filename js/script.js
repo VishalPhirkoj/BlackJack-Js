666b75fe58f60fa25874b69c7335b6e42d696976
VishalPhirkoj <77499770+VishalPhirkoj@users.noreply.github.com>
2022-02-06 13:03:26 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -27,7 +27,6 @@ if (localStorage.wallatbal == undefined) {
 } else if (localStorage.wallatbal) {
   myWalletbal5.innerHTML = parseInt(localStorage.wallatbal);
 } else {
-  //console.log("hey hey");
   localStorage.wallatbal = 1000;
   document.getElementById("myWalletbal5").innerHTML == 1000;
 }
